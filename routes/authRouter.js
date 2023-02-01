e61be8e6db2763296ab72e62706b5b00558f3fa0
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-01 20:21:49 -0300

    rutas y validaciones

---
+++ routes/authRouter.js
@@ -0,0 +1,22 @@
+//Rutas
+//auth
+// hostname + /auth
+
+const {Router} = require('express');
+const router = Router();
+const {check} = require('express-validator')
+
+router.post('/login',[
+check('password','la password debe ser de minimo 6 caracteres').isLength({min:6}),
+check('email','debe ser un email').isEmail(),
+
+],);
+
+router.post('/register',[
+    check('name','debe ser un name').notEmpty(),
+    check('email','debe ser un email').isEmail(),
+    check('password','la password debe ser de minimo 6 caracteres').isLength({min:6})
+    
+    ],);
+
+router.get('/renew',)
