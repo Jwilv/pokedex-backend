32ca647c68f4f353449d66ff5b3374f5e892ff87
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-04 20:24:20 -0300

    avance en createUSer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,41 @@
-const {response} = require('express')
+const { response } = require('express');
+const User = require('../models/User');
 
 const loginUser = (req, res = response) => {
-res.json({
-    ok:true,
-    msg:'user log'
-})
+
 }
 
 const createUser = (req, res = response) => {
-    res.json({
-        ok:true,
-        msg:'user create'
-    })
+    try {
+
+        const { email, password } = req.body;
+
+        let user = User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'el email que ingreso ya esta en uso'
+            })
+        }
+
+        user = new User(req.body);
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'pongase en contacto con nosotros'
+        })
+    }
 }
 
-const renewToken = (req, res = response)=>{
-res.json({
-    ok:true,
-    msg:'renew token'
-})
+const renewToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'renew token'
+    })
 }
 
 
