8c348ec0876edcaf7ce151679f15aef1b32cfbf5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-04 21:00:52 -0300

    se amplio el tiempo de vida del token

--- helpers/generateJwt.js
+++ helpers/generateJwt.js
@@ -6,7 +6,7 @@ const generateJwt = (uid, name) => {
     return new Promise((resolve, reject) => {
         const payload = { uid, name }
         jwt.sign(payload, process.env.SECRET_JWT_SEED, {
-            expiresIn: '2h'
+            expiresIn: '12h'
         }, (err, token) => {
             if (err) {
                 console.log(err);
